f4be7b565b880890757cb7e75ec60c7c078ae0d0
Viacheslav <skavyacheslav@gmail.com>
2020-05-25 19:59:14 +0300

    123

--- headerButtons.js
+++ headerButtons.js
@@ -1,16 +1,10 @@
 
-let menuButton = document.querySelector('.menu-icon');
-let sidebarScreen = document.querySelector('.sidebar-screen');
-let textScreen = document.querySelector('.text-screen');
+let menuButton = document.querySelector('.menu-button');
+let sidebar = document.querySelector('.sidebar');
 
 menuButton.onclick = function() {
-  sidebarScreen.classList.toggle('sidebar-opened')
-  if (sidebarScreen.classList.contains('sidebar-opened')) {
-    sidebarScreen.style.width = "500px";
-    textScreen.style.marginLeft = "500px";
-  } else {
-    sidebarScreen.style.width = "0";
-    textScreen.style.marginLeft= "0";
-  };
-};
+  sidebar.classList.toggle('sidebar--opened')
+}
+
+
 
