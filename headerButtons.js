0b63e17608b955831a594809f5fc388ff2b8d86f
Viacheslav <skavyacheslav@gmail.com>
2020-05-27 20:37:53 +0300

    sidebar structure, cards, footer

--- headerButtons.js
+++ headerButtons.js
@@ -3,8 +3,12 @@ let menuButton = document.querySelector('.menu-button');
 let sidebar = document.querySelector('.sidebar');
 
 menuButton.onclick = function() {
-  sidebar.classList.toggle('sidebar--opened')
+  sidebar.classList.toggle('sidebar--opened');
+  menuButton.classList.toggle('button--rotated');
+  menuButton.classList.toggle('button--active'); 
 }
+
+
 
 
 
